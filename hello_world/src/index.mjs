cb61be609ec3e05ab462740421f0a4f559c3d5f4
Cool233777 <cool232777@gmail.com>
2025-02-07 14:49:05 -0600

    Conexion rds con node

---
+++ hello_world/src/index.mjs
@@ -0,0 +1,106 @@
+import { getClient, transaction } from "./db-client.mjs";
+import format from "pg-format";
+
+const SCHEMA = "public";
+const TABLE = "factura";
+
+/**
+ * body: {
+ * fecha: string,
+ * total: number,
+ * detalles: [
+ *  {
+ *    nombre_producto: string,
+ *    cantidad: number,
+ *    precio: number
+ *  }
+ * ]
+ * }
+ */
+
+export const handler = async (event, context) => {
+  // TODO implement
+  if (event.httpMethod !== "GET") {
+    throw new Error(
+      `getMethod only accept GET method, you tried: ${event.httpMethod}`
+    );
+  }
+
+  console.info("received:", event);
+  let client;
+  try {
+    client = getClient();
+    await client.connect();
+    console.info("connected to database");
+
+    const transactionResult = transaction(client, async () => {
+      const body = JSON.parse(event.body);
+
+      const facturaQuery = `insert into ${SCHEMA}.factura (fecha, total) values ($1, $2) returning id_factura;`;
+
+      const values = [new Date(body.fecha), body.total];
+
+      const facturaResult = await client.query(facturaQuery, values);
+      const id_factura = facturaResult.rows[0].id_factura;
+
+      if (!id_factura) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify("Error al insertar factura"),
+        };
+      }
+
+      if (!body.detalles || body.detalles.length === 0) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify("Error detalles are empty"),
+        };
+      }
+
+      const detalles = body.detalles.map((detalle) => {
+        return [
+          id_factura,
+          detalle.nombre_producto,
+          detalle.cantidad,
+          detalle.precio,
+        ];
+      });
+
+      const detalleQuery = format(
+        `insert into ${SCHEMA}.detalle_factura (id_factura, nombre_producto, cantidad, precio) values %L`,
+        detalles
+      );
+
+      const detalleResult = await client.query(detalleQuery);
+
+      if (detalleResult?.rows?.length !== body?.detalles?.length) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify("Error al insertar detalles"),
+        };
+      }
+
+      return detalleResult.rows;
+    });
+
+    if (transactionResult.error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(transactionResult.error),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(transactionResult.result),
+    };
+  } catch (err) {
+    console.error("connection error", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err),
+    };
+  } finally {
+    client?.end();
+  }
+};
---
+++ hello_world/src/rds_connect_node.mjs
@@ -0,0 +1,35 @@
+import { Client } from 'pg';
+
+export function getClient() {
+	return new Client({
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+    });
+}
+
+export async function transaction(client, func) {
+	try {
+		console.info("Beginning transaction...");
+		await client.query("BEGIN");
+		const result = await func();
+
+		// This means is an HTTP response.
+		if (result.response) {
+			console.info("Rolling back transaction due to HTTP response...");
+			await client.query("ROLLBACK");
+			return result;
+		}
+
+		console.info("Comitting transaction...");
+		await client.query("COMMIT");
+		return { result };
+	} catch (error) {
+		console.error({ error }, "Rolling back transaction due to error...");
+		await client.query("ROLLBACK");
+		return { error };
+	}
+} 
+
